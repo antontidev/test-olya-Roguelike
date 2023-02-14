1c3b3eeb398961cdc1eeb473ede74caa86495a43
dawr <modina-o@mail.ru>
2023-02-14 16:27:56 +0500

    Декор карты уровней. Не работает кнопка

--- Assets/Scripts/Map/MapController.cs
+++ Assets/Scripts/Map/MapController.cs
@@ -9,11 +9,13 @@ namespace Map
         [Header("Герой")]
         public HeroMap Hero;
         [SerializeField] private GameObject _heroMapPrefab;
+        public Transform Camera;
 
         public void Start()
         {
             GameController.Instance.MapController = this;
             Hero.transform.position = GameController.Instance.transform.position;
+            Camera.transform.position = new Vector3(0, Hero.transform.position.y + 4, -10);
         }
 
         public void ChangeOnNextLevel()
